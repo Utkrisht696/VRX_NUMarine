0c4b3f73d54043eb3dc59c9a386aea2ba72c5b44
Utkrisht <c3339567@uon.edu.au>
2024-07-07 06:33:52 +1000

    testing

--- install/share/gps_odom_pkg/launch/localization_launch.py
+++ install/share/gps_odom_pkg/launch/localization_launch.py
@@ -1,11 +1,10 @@
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, LogInfo
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import launch_ros.actions
 import os
 import launch.actions
-
 
 def generate_launch_description():
     pkg_share = get_package_share_directory('gps_odom_pkg')
@@ -18,28 +17,31 @@ def generate_launch_description():
         launch.actions.DeclareLaunchArgument(
             'output_location', default_value='~/dual_ekf_navsat_example_debug.txt'
         ),
+        LogInfo(msg="Launching ekf_filter_node_odom..."),
         launch_ros.actions.Node(
             package='robot_localization',
             executable='ekf_node',
             name='ekf_filter_node_odom',
             output='screen',
-            parameters=[rl_params_file, {'use_sim_time': True}],
+            parameters=[rl_params_file],
             remappings=[('odometry/filtered', 'odometry/local')],
         ),
+        LogInfo(msg="Launching ekf_filter_node_map..."),
         launch_ros.actions.Node(
             package='robot_localization',
             executable='ekf_node',
             name='ekf_filter_node_map',
             output='screen',
-            parameters=[rl_params_file, {'use_sim_time': True}],
+            parameters=[rl_params_file],
             remappings=[('odometry/filtered', 'odometry/global')],
         ),
+        LogInfo(msg="Launching navsat_transform..."),
         launch_ros.actions.Node(
             package='robot_localization',
             executable='navsat_transform_node',
             name='navsat_transform',
             output='screen',
-            parameters=[rl_params_file, {'use_sim_time': True}],
+            parameters=[rl_params_file],
             remappings=[
                 ('imu/data', '/wamv/sensors/imu/imu/data'),
                 ('gps/fix', '/wamv/sensors/gps/gps/fix'),
@@ -48,4 +50,28 @@ def generate_launch_description():
                 ('odometry/filtered', 'odometry/global'),
             ],
         ),
+        LogInfo(msg="Launching static_transform_publisher_base_link..."),
+        launch_ros.actions.Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_transform_publisher_base_link',
+            arguments=['--x', '0', '--y', '0', '--z', '0', '--roll', '0', '--pitch', '0', '--yaw', '0', '--frame-id', 'world', '--child-frame-id', 'wamv/wamv/base_link'],
+            output='screen'
+        ),
+        LogInfo(msg="Launching static_transform_publisher_imu..."),
+        launch_ros.actions.Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_transform_publisher_imu',
+            arguments=['--x', '0', '--y', '0', '--z', '1', '--roll', '0', '--pitch', '0', '--yaw', '0', '--frame-id', 'wamv/wamv/base_link', '--child-frame-id', 'wamv/sensors/imu/imu/data'],
+            output='screen'
+        ),
+        LogInfo(msg="Launching static_transform_publisher_gps..."),
+        launch_ros.actions.Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_transform_publisher_gps',
+            arguments=['--x', '0', '--y', '0', '--z', '2', '--roll', '0', '--pitch', '0', '--yaw', '0', '--frame-id', 'wamv/wamv/base_link', '--child-frame-id', 'wamv/sensors/gps/gps/fix'],
+            output='screen'
+        )
     ])
